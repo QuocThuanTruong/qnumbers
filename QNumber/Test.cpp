4b2b42d51d5e0849ef07b43f6166cac518862938
annalla <danghongxuyen2000@gmail.com>
2020-04-26 12:46:50 +0700

    update qint

--- QNumber/Test.cpp
+++ QNumber/Test.cpp
@@ -6,17 +6,34 @@ int main()
 {
 	//TO DO
 	//Test here
-	string a = "5579";
-	string b = "240";
+	string a = "5879";
+	string b = "-15";
 	string c = "11110111";
 
 	QInt q;
 	q.scanQInt(b, 10);
-	q.printQInt(16);
-
-
+	q.printQInt(10);
+	
 	q.scanQInt(c, 2);
 	q.printQInt(10);
+
+	QInt add1;
+	QInt add2;
+	QInt kq;
+	a="1";
+	b="16";
+	add1.scanQInt(a, 10);
+	add2.scanQInt(b, 10);
+	/*
+	if(add1>add2)
+		cout<<"lon hon\n";
+	if(add1<add2)
+		cout<<"nho hon\n";
+	*/
+	kq = add1 - add2;
+	//kq=kq-add1;
+	kq.printQInt(10);
+	
 
 	return 0;
 }
