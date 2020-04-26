2c8ab6ea4dccac4ceba0b3065d02b539e30ae0c2
annalla <50328046+annalla@users.noreply.github.com>
2020-04-26 15:23:09 +0700

    Update Test.cpp

--- QNumber/Test.cpp
+++ QNumber/Test.cpp
@@ -6,52 +6,18 @@ int main()
 {
 	//TO DO
 	//Test here
-	string a = "5579";
+	string a = "5579";				
 	string b = "240";
-
-	cout << "a: " << a << endl;
-	cout << "b: " << b << endl;
-
-	QInt c;
-	c.scanQInt(a, 10);
-	cout << "a: ";
-	c.printQInt(16);
-
-	QInt d;
-	d.scanQInt(b, 10);
-	cout << "b: ";
-	d.printQInt(2);
+	string c = "11110111";	
 
 
-	cout << "a & b\n";
-	(c & d).printQInt(2);
+	QInt q;		
+	q.scanQInt(b, 10);		
+	q.printQInt(16);	
 
-	cout << "a | b\n";
-	(c | d).printQInt(2);
 
-	cout << "a ^ b\n";
-	(c ^ d).printQInt(2);
-
-	cout << "~a\n";
-	(~c).printQInt(2);
-	
-	cout << "a >> 2\n";
-	(c >> 2).printQInt(10);
-
-	cout << "b << 4\n";
-	(d << 2).printQInt(10);
-
-	c.rol();
-	cout << "rol a\n";
-	c.printQInt(2);
-
-	d.ror();
-	cout << "ror b\n";
-	d.printQInt(2);
-
-	
-	cout << IntegerStringUtils::convertFractionPartToBin("3017578125") << endl; //0.3017578125
-
+	q.scanQInt(c, 2);		
+	q.printQInt(10);
 	
 
 	return 0;
